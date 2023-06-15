06a8b1c4986e6f28dd28cfd4b693556f27b35ee2
Brian Chege Gichuhi <brianchegegichuhi@gmail.com>
2023-06-15 07:00:21 -0400

    configuring the env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WalletModule } from './wallet/wallet.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [WalletModule],
+  imports: [ConfigModule.forRoot(), WalletModule],
   controllers: [AppController],
   providers: [AppService],
 })
